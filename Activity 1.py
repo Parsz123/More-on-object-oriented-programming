cfba2f48861bdfd0bd85587009c0a541c850b619
Parsz123 <hetaldr@yahoo.com>
2025-12-28 11:09:37 +0000

    hello

---
+++ Activity 1.py
@@ -0,0 +1,14 @@
+class I0String():
+    def __init__(self):
+        self.str1 = ""
+    
+    def get_string(self):
+        self.str1 = input("Enter string:")
+    
+    def print_string(self):
+        print("Result is:", self.str1.upper())
+
+str1 = I0String()
+
+str1.get_string
+str1.print_string
